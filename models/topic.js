41c3c16545418899ce537faeee7144a75322f6b5
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-09-27 19:36:25 -0500

    Continue view refactoring

--- models/topic.js
+++ models/topic.js
@@ -12,14 +12,6 @@ export default class Topic {
         this.threads = threads;
     }
 
-    /**
-     * 
-     * @returns {number} The total number of threads
-     */
-    get_thread_count() {
-        return this.threads.length;
-    }
-
     get_longest_thread_length() {
         var lengths = this.threads.map((thread) => thread.get_length());
         return Math.max(...lengths);
--- views/stripmall/stripmall.js
+++ views/stripmall/stripmall.js
@@ -3,36 +3,6 @@ const { render } = pkg;
 import Topic from '../../models/topic.js';
 
 /**
- * @param {Topic} topic
- * @returns {width: number, height: number}
- */
-function topic_dimensions(topic) {
-    return {
-        width: (2 * (topic.get_thread_count() + 2)),
-        height: (2 * (topic.get_longest_thread_length() + 2))
-    };
-}
-
-/**
- * @param {Topic[]} topics
- * @returns {string}
- */
-function build_strip(topics) {
-    var output = '';
-    var offset = 0;
-    for (var topic of topics) {
-        output += `
-            <a-entity class="topic" position="${offset} 0 0">
-                ${render_topic(topic)}
-            </a-entity>
-        `;
-        offset += topic_dimensions(topic).width + 2;
-    }
-
-    return output;
-}
-
-/**
  * @returns {string}
  */
 export function page_full(topics) {
@@ -46,6 +16,8 @@ export function page_full(topics) {
  * @returns string
  */
 export function page_fragment(topics) {
+    return render('./views/stripmall/stripmall.html', { topics });
+
     // Split the topics into two groups
     var left_topics = [];
     var right_topics = [];
@@ -59,14 +31,18 @@ export function page_fragment(topics) {
     right_topics.reverse();
 
     // Build each group
-    var left_strip = build_strip(left_topics);
-    var right_strip = build_strip(right_topics);
+    var left_strip = render('./views/stripmall/strip.html', {
+        topics: left_topics
+    });
+    var right_strip = render('./views/stripmall/strip.html', {
+        topics: right_topics
+    })
 
     // Determine how far the right strip needs to be moved out further
     var offset = 0;
     for (let topic of right_topics) {
         // Account for each topic area width
-        offset -= topic_dimensions(topic).width;
+        offset -= 2 * (topic.threads.length + 2);
     }
     //Account for spacing between topic areas
     offset -= 2 * (right_topics.length - 1);
@@ -82,35 +58,4 @@ export function page_fragment(topics) {
     `;
 
     return scene;
-}
-
-/**
- * @param {Topic} topic 
- * @return {string}
- */
-function render_topic(topic) {
-    const { width, height } = topic_dimensions(topic);
-
-    var output = render('./views/stripmall/topic.html', {topic, width, height});
-
-    // Render threads
-    var x_offset = 1;
-    for (var thread of topic.threads) {
-        output += `
-            <a-entity position="${x_offset} 0 -1">
-                ${render('./views/stripmall/thread.html', {thread})}
-            </a-entity>
-        `;
-        x_offset += 2;
-    }
-
-    // Render post form
-    output += `
-        <a-entity position="${x_offset} 0 -1">
-            ${render('./views/stripmall/form.html', {
-                form_title: 'New Post', title_field: true})}
-        </a-entity>
-    `
-
-    return output;
 }
