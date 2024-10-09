244c88c8afd42e13c2c0157d45264b103d65110d
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-09 12:21:40 -0500

    Comment and cleanup form component

--- src/client/components/form.ts
+++ src/client/components/form.ts
@@ -1,22 +1,36 @@
 'use strict';
 
 AFRAME.registerComponent('form', {
-    _inputs: [] as Element[],
-    _tab_order: [] as number[],
-    _selected: 0,
+    _inputElements: [] as Element[],
+    _tabOrder: [] as number[],
+    _tabOrderIndex: 0,
 
+    // Initalize the form in the scene
     init: function(): void {
         // Disable wasd movement and the cursor
         document.querySelector('[camera]').removeAttribute('wasd-controls');
         document.querySelector('#cursor').removeAttribute('cursor');
 
-        // Update the list of child input components
-        this._updateInputs();
+        // Construct the form's list of input elements and tab order
+        this._inputElements = [...this.el.querySelectorAll('[input]')];
+        this._tabOrder = [];
+        for (var i = 0; i < this._inputElements.length; i++) {
+            var input = this._inputElements[i];
+            var type = input.getAttribute('type');
+            if (type !== 'hidden') {
+                this._tabOrder.push(i);
+            }
+        }
+
+        // Focus the first focusable input on the form
+        this._tabOrderIndex = 0;
+        this.getSelectedInputComponent().focus();
 
         // Listen for key presses
         window.addEventListener('keydown', this._processKeyboardEvent);
     },
 
+    // Remove the form
     remove: function() {
         // Remove the form from the DOM
         document.querySelector('[formmount]').innerHTML = '';
@@ -29,37 +43,22 @@ AFRAME.registerComponent('form', {
         window.removeEventListener('keydown', this._processKeyboardEvent);
     },
 
-    _updateInputs: function(): void {
-        // Construct the form's list of inputs and tab order
-        this._inputs = [...this.el.querySelectorAll('[input]')];
-        this._tab_order = [];
-        for (var i = 0; i < this._inputs.length; i++) {
-            var input = this._inputs[i];
-            var type = input.getAttribute('type');
-            if (type !== 'hidden') {
-                this._tab_order.push(i);
-            }
-        }
-
-        // Focus the first focusable input on the form
-        this._selected = 0;
-        this.getSelectedInputComponent().focus();
-    },
-
+    // Control tabbing between form inputs
     tab(backwards: boolean): void {
         // Unfocus the originally selected component
         var selectedComponent = this.getSelectedInputComponent();
         selectedComponent.unfocus();
 
-        // Modify the selection index
+        // If the user tabs backwards, decrement the tab index
         if (backwards) {
-            this._selected--;
-            if (this._selected < 0) {
-                this._selected = this._tab_order.length - 1;
+            this._tabOrderIndex--;
+            if (this._tabOrderIndex < 0) {
+                this._tabOrderIndex = this._tabOrder.length - 1;
             }
+        // If the user tabs forwards, increment the tab index
         } else {
-            this._selected++;
-            this._selected %= this._tab_order.length;
+            this._tabOrderIndex++;
+            this._tabOrderIndex %= this._tabOrder.length;
         }
 
         // Focus the newly selected component
@@ -67,31 +66,32 @@ AFRAME.registerComponent('form', {
         selectedComponent.focus();
     },
 
+    // Get the A-Frame input component cooresponding to the selected input
     getSelectedInputComponent(): any {
-        // Get the A-Frame input component cooresponding to the selected input
-        var index: number = this._tab_order[this._selected];
-        var element: Element = this._inputs[index];
+        var index: number = this._tabOrder[this._tabOrderIndex];
+        var element: Element = this._inputElements[index];
         var component = (element as any).components.input;
         return component;
     },
 
+    // Process keyboard events passed to the form
     _processKeyboardEvent: function(event: KeyboardEvent): void {
         event.preventDefault();
 
         // Get the form component
         var form = document.querySelector('[form]').components.form;
 
-        // Tab between focusable inputs
+        // If the user presses tab (and possibly shift), tab between focusable inputs
         if (event.key == 'Tab') {
             form.tab(event.shiftKey);
-            return;
+        // Otherwise, forward the keyboard event to the selected input
+        } else {
+            var input_component = form.getSelectedInputComponent();
+            input_component.processKeyboardEvent(event);
         }
-
-        // Forward the keyboard event to the selected control
-        var input_component = form.getSelectedInputComponent();
-        input_component.processKeyboardEvent(event);
     },
 
+    // Attempt to submit the form
     submit: async function() {
         // Get all the inputs with name attributes
         var inputs: Element[] = [...this.el.querySelectorAll('[input]')]
